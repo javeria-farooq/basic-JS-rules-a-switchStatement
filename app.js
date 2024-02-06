236b1cf14afa2310bbc30b664a0f436ef3e3e146
javeria-farooq <gettingeducation6@gmail.com>
2024-02-07 01:20:21 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//        //    explore basic rules of JavaScript:
+
+
+// document.write("<h1>" + 123 + "</h1>")
+// document.write('<h1> hello world </h1>')
+
+// alert("Your application has been submitted")
+// document.write('hello' + '<br>' + 'pakistan')
+// document.write('hello' + '\n' + 'pakistan')
+// alert('<h1>' + 'hello world' + '</h1>')
+// alert('hello' + '\n' + 'world')
+
+//             //A switch expression: 
+
+// const expression = "Apple";
+// switch (expression) {
+//   case "Apple"
+//     console.log("Apple are sold in pkr");
+//     break;
+//   case "banana"
+//     console.log("banana are also sold in pkr");
+//     break;
+//   case "Mango"
+//     console.log("Mango are sold in pkr also")
+// }
